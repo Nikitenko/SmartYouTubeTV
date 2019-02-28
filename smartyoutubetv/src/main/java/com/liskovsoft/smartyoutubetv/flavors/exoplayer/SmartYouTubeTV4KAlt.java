c7a946b6b647d85cf6ca71ab1a48f67a0a17c1f0
Nikitenko <vitaly.nikitenko@eltex.loc>
2019-02-28 13:38:42 +0700

    setup default app preferences

--- smartyoutubetv/src/main/java/com/liskovsoft/smartyoutubetv/flavors/exoplayer/SmartYouTubeTV4KAlt.java
+++ smartyoutubetv/src/main/java/com/liskovsoft/smartyoutubetv/flavors/exoplayer/SmartYouTubeTV4KAlt.java
@@ -1,5 +1,8 @@
 package com.liskovsoft.smartyoutubetv.flavors.exoplayer;
 
+import android.os.Bundle;
+
+import com.liskovsoft.smartyoutubetv.common.prefs.SmartPreferences;
 import com.liskovsoft.smartyoutubetv.fragments.BrowserFragment;
 import com.liskovsoft.smartyoutubetv.flavors.common.TwoFragmentsManagerActivity;
 import com.liskovsoft.smartyoutubetv.flavors.exoplayer.fragments.SmartYouTubeTV4KAltFragment;
@@ -14,6 +17,16 @@ public class SmartYouTubeTV4KAlt extends TwoFragmentsManagerActivity {
         // create fragments in constructor so there will be more time for initialization
         mBrowser = new SmartYouTubeTV4KAltFragment();
         mPlayer = new ExoPlayerFragment();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        SmartPreferences prefs = SmartPreferences.instance(this);
+        prefs.setEnableOKPause(false);
+        prefs.setEnableEndCards(false);
+        prefs.setBootstrapUpdateCheck("update_check_disabled");
+        prefs.setBootstrapSaveSelection(false);
     }
 
     @Override
